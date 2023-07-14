8683b1394be6ce2e72be50d87f21834eb4f4a6d7
Lorena Sales <lorenassales@gmail.com>
2023-07-14 19:03:35 +0000

    store Excluded

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -9,16 +9,24 @@ with
         from {{ ref('stg_sap__customers') }}
     )
 
+    , stg_stores as (
+        select *
+        from {{ ref('stg_sap__stores') }}
+    )
+
     , dim_customers as (
         select
-            c.customer_id            
-            , p.person_name as customer_name           
+            c.customer_id
+            , s.store_id
+            , p.person_name as customer_name
+            , s.store_name                                     
         from stg_customers c
         left join stg_people p on
-            c.customer_id = p.person_id       
+            c.customer_id = p.person_id
+        left join stg_stores s on
+            c.store_id = s.store_id
     )
 select 
     {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as customer_sk
     , *
 from dim_customers
-	
